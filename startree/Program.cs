ed4c8006c07854e22e5f28932e5284b1e6d5a636
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-12 20:07:02 +0900

    06번 예제 문제 풀이

--- startree/Program.cs
+++ startree/Program.cs
@@ -10,6 +10,20 @@ namespace startree
     {
         static void Main(string[] args)
         {
+            for(int i = 0; i < 8; i++)
+            {
+                for (int j = 8; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for(int k = 0; k < i * 2 + 1; k++)
+                {
+                    Console.Write("*");
+                }
+                
+                Console.WriteLine();
+            }
         }
     }
 }
